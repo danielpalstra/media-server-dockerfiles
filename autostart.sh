879d8356c299837a0fd4ca1aa89211748286b9c5
Jeff Sturgis <jeffsturgis@gmail.com>
2014-06-04 18:07:11 -0700

    updated readme and changes docker config location

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install -y supervisor
 cp ./mediastart.conf /etc/supervisor/conf.d
-sh -c "echo 'DOCKER_OPTS=\"-r=false\"' > /etc/default/docker"
+sh -c "echo 'DOCKER_OPTS=\"-r=false\"' > /etc/default/docker.io"
