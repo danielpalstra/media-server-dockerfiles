bfb782fe8c23d042fe12192401385f942a0d92d6
Jeff Sturgis <jeffsturgis@gmail.com>
2015-04-29 16:27:52 -0700

    replace sickbeard with sonarr

--- start.sh
+++ start.sh
@@ -4,19 +4,22 @@ STORAGEDIR=/media/jeff/nas
 TMPDIR=/media/jeff/storage/tmp
 DLDIR=/media/jeff/storage/downloads
 
-echo Starting docker containers \n
+echo Starting docker containers
 
 echo Starting couchpotato...
 docker run --name couchpotato -d -p 5050:5050 -v /etc/localtime:/etc/localtime:ro -v $STORAGEDIR:/mnt/nas:rw -v $DLDIR:/mnt/downloads:rw -v $DATADIR/couchpotato:/mnt/state:rw couchpotato-image
 
-echo Starting sickbeard...
-docker run --name sickbeard -d -p 8081:8081 -v /etc/localtime:/etc/localtime:ro -v $STORAGEDIR:/mnt/nas:rw -v $DLDIR:/mnt/downloads:rw -v $DATADIR/sickbeard:/mnt/state:rw sickbeard-image
+# echo Starting sickbeard...
+# docker run --name sickbeard -d -p 8081:8081 -v /etc/localtime:/etc/localtime:ro -v $STORAGEDIR:/mnt/nas:rw -v $DLDIR:/mnt/downloads:rw -v $DATADIR/sickbeard:/mnt/state:rw sickbeard-image
+
+echo Starting Sonarr...
+docker run --name sonarr -d -p 8989:8989 -u root -v /etc/localtime:/etc/localtime:ro -v $STORAGEDIR:/volumes/media:rw -v $DLDIR:/volumes/completed:rw -v $DATADIR/sonarr:/volumes/config:rw tuxeh/sonarr
 
 echo Starting headphones...
 docker run --name headphones -d -p 8181:8181 -v /etc/localtime:/etc/localtime:ro -v $TMPDIR:/mnt/tmp:rw -v $STORAGEDIR:/mnt/nas:rw -v $DLDIR:/mnt/downloads:rw -v $DATADIR/headphones:/mnt/state:rw headphones-image
 
 echo Starting sabnzbd...
-docker run --name sabnzbd --link sickbeard:sb -d -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v $DLDIR:/mnt/downloads:rw -v $DATADIR/sabnzbd:/mnt/state:rw -v $STORAGEDIR:/mnt/nas:rw -v $TMPDIR:/mnt/tmp:rw sabnzbd-image
+docker run --name sabnzbd -d -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v $DLDIR:/mnt/downloads:rw -v $DATADIR/sabnzbd:/mnt/state:rw -v $STORAGEDIR:/mnt/nas:rw -v $TMPDIR:/mnt/tmp:rw sabnzbd-image
 
 echo Starting deluge...
 docker run --name deluge -d -p 58846:58846 -p 8112:8112 -v /etc/localtime:/etc/localtime:ro -v $TMPDIR:/mnt/tmp:rw -v $DATADIR/deluge:/mnt/state:rw -v $DLDIR:/mnt/downloads:rw deluge-image
--- stop.sh
+++ stop.sh
@@ -5,14 +5,18 @@ SICK=$(sudo docker ps | grep "sickbeard" | awk '{print $1}')
 HEAD=$(sudo docker ps | grep "headphones" | awk '{print $1}')
 SAB=$(sudo docker ps | grep "sabnzbd" | awk '{print $1}')
 DELUGE=$(sudo docker ps | grep "deluge" | awk '{print $1}')
+SONARR=$(sudo docker ps | grep "sonarr" | awk '{print $1}')
 
 echo Stopping docker containers
 
 echo Stopping couchpotato...
 docker stop $COUCH
 
-echo Stopping sickbeard...
-docker stop $SICK
+# echo Stopping sickbeard...
+# docker stop $SICK
+
+echo Stopping sonarr
+docker stop $SONARR
 
 echo Stopping headphones...
 docker stop $HEAD
