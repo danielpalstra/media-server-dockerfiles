03c004b4fb999b423cf7e17483f348aeb777931f
Jeff Sturgis <jeffsturgis@gmail.com>
2015-07-19 16:34:55 -0700

    Use docker-compose to manage containers

--- autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get install -y supervisor
-cp ./mediastart.conf /etc/supervisor/conf.d
-supervisorctl reread
-supervisorctl update
-supervisorctl
-sh -c "echo 'DOCKER_OPTS=\"-r=false\"' > /etc/default/docker.io"
-sh -c "echo 'DOCKER_OPTS=\"-r=false\"' > /etc/default/docker"
